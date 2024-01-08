97b722fe7a84b3b898c5f03ef91991f87bbea1bf
JormaByte <joel.oksanen1223@gmail.com>
2024-01-08 14:19:19 +0200

    adding components

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,10 +1,16 @@
 import {TestComponent} from './components/TestComponent'
+import {AddExercise} from './components/AddExercise'
+import {ExerciseList} from './components/ExerciseList'
+import {Settings} from './components/Settings'
 
 function App() {
   
   return (
     <div>
       <TestComponent/>
+      <AddExercise />
+      <ExerciseList />
+      <Settings />
     </div>
   );
 }
