1899c852433ed923a63fc99400a0f172cc9bfb09
JormaByte <joel.oksanen1223@gmail.com>
2024-01-09 14:03:04 +0200

    installing rn paper

--- assignment/src/components/AddExercise.js
+++ assignment/src/components/AddExercise.js
@@ -1,10 +1,22 @@
 export function AddExercise(){
 
-console.log("works")
+    console.log("works")
+
+    let tiedot = date + " " + nimi + " -> " + teksti ;
+  
+  
+    const newDiv = document.createElement("div") ;
+
+
+    newDiv.textContent = tiedot;
+
+    document.getElementById("p").appendChild(newDiv) ;
 
     return(
         <div class='add'>
             <h2>Tell us about your last exercise here:</h2>
+
+
             <button>Add Exercise</button>
         </div>
     )
