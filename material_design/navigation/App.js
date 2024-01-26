2a59a0434ec0776ec6e15a428e53c8a64b88269c
JormaByte <joel.oksanen1223@gmail.com>
2024-01-26 13:43:27 +0200

    transferring value across stack screens with useState

--- material_design/navigation/App.js
+++ material_design/navigation/App.js
@@ -1,14 +1,60 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>NAV EXAMPLE !!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{title: 'User Page', headerStyle: {backgroundColor: 'bisque'}}}>
+        <Stack.Screen name='Home' component={Home}/>
+        <Stack.Screen name='User' component={User} />
+        <Stack.Screen name='Settings' component={Settings}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+function Home({navigation}){
+
+  const [userName, setUserName] = useState('')
+
+  return(
+    <View>
+      <TextInput placeholder='WHAT IS YOUR NAME?' onChangeText={setUserName}></TextInput>
+
+      <Button title='Go to the User page' onPress={() => navigation.navigate('User', {userName})} />
+      </View>
+  )
+}
+
+function User({navigation, route}){
+  const username = route.params.userName
+  return(
+    <Text style={styles.text} onPress={() => navigation.navigate('Settings')}>
+      The current user: {username}
+    </Text>
+  )
+}
+
+function Settings({navigation}){
+  return(
+    <Text style={styles.text} onPress={() => navigation.navigate('Home')}>
+      GO HOME
+    </Text>
+  )
+}
+
+
+
+
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +63,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize: 24, alignItems: 'stretch', borderWidth: 2, borderRadius: 9, padding: 8
+  }
 });
